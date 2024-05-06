8c5585494a753fa4970b6f623917c04254c456d9
zhi-ac <1252580622@qq.com>
2024-05-06 13:18:31 +0800

    添加Dubbo，使用Nacos作为注册中心，RPC远程调用测试

--- fengapi-gateway/src/main/java/com/feng/fengapigateway/FengapiGatewayApplication.java
+++ fengapi-gateway/src/main/java/com/feng/fengapigateway/FengapiGatewayApplication.java
@@ -1,29 +1,53 @@
 package com.feng.fengapigateway;
 
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.stereotype.Service;
+import com.feng.project.provider.DemoService;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class,
+        DataSourceTransactionManagerAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class})
+@EnableDubbo
+@Service
 public class FengapiGatewayApplication {
 
+    @DubboReference
+    private DemoService demoService;
+
     public static void main(String[] args) {
-        SpringApplication.run(FengapiGatewayApplication.class, args);
+
+        ConfigurableApplicationContext context = SpringApplication.run(FengapiGatewayApplication.class, args);
+        FengapiGatewayApplication application = context.getBean(FengapiGatewayApplication.class);
+        String result = application.doSayHello("world");
+        String result2 = application.doSayHello2("world");
+        System.out.println("result: " + result);
+        System.out.println("result: " + result2);
     }
 
-    //@Bean
-    //public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-    //    // 创建路由规则的构建器
-    //    return builder.routes()
-    //            // 定义路由规则，给该规则起一个名字 "tobaidu"
-    //            .route("tobaidu", r -> r.path("/baidu")
-    //                    .uri("https://baidu.com"))
-    //            // 将满足 "/google" 路径的请求转发到 "www.google.com"
-    //            .route("tospring", r -> r.path("/spring")
-    //                    .uri("https://github.com/zhi-ac/fengapi-frontend"))
-    //            // 创建并返回路由规则配置对象
-    //            .build();
-    //}
+    public String doSayHello(String name) {
+        return demoService.sayHello(name);
+    }
+
+    public String doSayHello2(String name) {
+        return demoService.sayHello2(name);
+    }
+
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return builder.routes()
+//                .route("tobaidu", r -> r.path("/baidu")
+//                        .uri("https://www.baidu.com"))
+//                .route("toyupiicu", r -> r.path("/yupiicu")
+//                        .uri("http://yupi.icu"))
+//                .build();
+//    }
+
 }
